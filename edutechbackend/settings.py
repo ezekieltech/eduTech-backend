3f5876dd50bc9a98f94ad3489d8620a82faa88cf
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2021-01-19 03:37:56 +0100

    Update settings.py

--- edutechbackend/settings.py
+++ edutechbackend/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -168,10 +169,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+)
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
